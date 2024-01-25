214a20a4aac229dc2d863424d60fa73be76bfd33
Cleysson Freitas <cleyssonfreitas30@gmail.com>
2024-01-25 11:42:48 -0300

    Adicionado firebase ao index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCpvnepyKdaibyFoC1S1ZavHPWPuEzu4co",
+  authDomain: "lix-store.firebaseapp.com",
+  projectId: "lix-store",
+  storageBucket: "lix-store.appspot.com",
+  messagingSenderId: "368975085440",
+  appId: "1:368975085440:web:856c56f4885dcae2cacb65",
+  measurementId: "G-DKXGZGB66R"
+};
 
+const firebaseApp = initializeApp(firebaseConfig);
+
+export { firebaseApp, getFirestore };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
