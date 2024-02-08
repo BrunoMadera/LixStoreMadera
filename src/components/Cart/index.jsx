89708c6f64fb14bff734ddc478f5b072a68d1bad
BrunoMadera <brunomadera@gmail.com>
2024-02-07 23:07:05 -0300

    070224

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -7,6 +7,7 @@ import { CartContext } from "../../contexts/CartContext";
 import { useContext } from 'react';
 
 import { MdRemoveShoppingCart } from "react-icons/md";
+// import { CgRemoveR } from "react-icons/cg";
 import CartWidget from '../CartWidget';
 import { Link } from "react-router-dom";
 
@@ -28,10 +29,12 @@ function Cart() {
                     {carr.map((carr, index) => { 
                       return (
                         <div key={index} style={{backgroundColor:'white'}} className="h13" >
+                          {/* <CgRemoveR className="removeItem" onClick={removeItem}/> */}
                           <p style={{fontWeight:'bold'}}>{carr.name} 
                             <span style={{fontWeight:'normal'}} > x {(carr.count)}</span>
                             <span style={{fontWeight:'normal'}} > = R$ {parseFloat(carr.price * carr.count).toFixed(2)}</span>
                           </p>
+
                           <hr className="hrBrown" />
                         </div>
                       );
@@ -53,6 +56,12 @@ function sumPrice (){
         return parseFloat(sumPrice).toFixed(2);
       }
 
+// function removeItem(index){
+
+//   carr.splice({index},1);
+//   console.log(carr);
+//   return carr;
+// }
  
   const [show, setShow] = useState(false);
 
@@ -79,11 +88,17 @@ function sumPrice (){
             <p className="h11 sumTitle"> 
                 {cartContext.cartAdded.length === 0 ? 
                     <span>Carrinho Vazio.. <br/> Continue comprando :D </span> 
-                  : <span className="totalCart"> Total do Carrinho R$ {sumPrice()} </span>}
-            <Link to="/CheckOut"><Button variant="outlined" color="success" className="checkoutButton" endIcon={<ShoppingCartCheckoutIcon />} onClick={handleClose} > Ir para CheckOut </Button> </Link>
+                  : <span className="totalCart"> Total do Carrinho R$ {sumPrice()} </span>
+                }
+              <hr className="hrBrown" />  
             </p>
 
-
+            <p> {cartContext.cartAdded.length === 0 ? 
+                <Button variant="disabled" > </Button>  
+                : <Link to="/CheckOut"><Button variant="outlined" color="success" className="checkOutButton"endIcon={<ShoppingCartCheckoutIcon />} onClick={handleClose} > Ir para CheckOut </Button> </Link>
+                }
+            </p>
+            
            </Offcanvas.Body>
       </Offcanvas>
     </>
--- src/components/CheckOut/index.jsx
+++ src/components/CheckOut/index.jsx
@@ -1,10 +1,98 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+
+
+import { CartContext } from "../../contexts/CartContext";
+import { useContext } from 'react';
+
+import { TbCubeSend } from "react-icons/tb";
+
+import swal from 'sweetalert';
+
+
+const HtmlTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: 'rgba(16, 52, 166, 0.2)',
+    color: 'rgba(255, 255, 255, 0.87)',
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}));
+
+function confirmOrder(){
+
+  swal({
+  title: "Feito!",
+  text: "Seu pedido foi enviado! Entraremos em contato!",
+  button: false,
+
+});
+
+}
 
 function CheckOut() {
+
+const cartContext = useContext(CartContext)
+const carr = cartContext.cartAdded;
+
+function sumPrice (){
+
+        let sumPrice=0;
+        
+        for(var i = 0; i <cartContext.cartAdded.length; i++) { 
+              
+              sumPrice= cartContext.cartAdded[i].price * cartContext.cartAdded[i].count+ sumPrice; 
+        } 
+        return parseFloat(sumPrice).toFixed(2);
+      }
 
 return(
 <>
 
-<h1 style={{marginTop:'150px', color:'white'}}>checkout page</h1>
+<div className="checkOutBox h13">Aqui estão os Produtos do Seu Carrinho
+    <div className="checkOutContainer">
+        {carr.map((carr, index) => { 
+        return (
+
+            <div key={index} style={{backgroundColor:'white'}} className="h13 checkOutItem" >
+                <p style={{fontWeight:'bold'}}>{carr.name} 
+                    <span style={{fontWeight:'normal'}} > x {(carr.count)}</span>
+                    <span style={{fontWeight:'normal'}} > = R$ {parseFloat(carr.price * carr.count).toFixed(2)}</span>
+                </p>
+                <hr className="hrBrown" />
+            </div>
+        );
+        })}
+    </div>
+    <hr className="hrPink" />
+
+    <div className="containerCheckSum">
+    <span className="totalCartCheckOut"> Total do Carrinho R$ {sumPrice()} </span>
+        <hr className="hrPink" /> 
+
+      <HtmlTooltip
+        title={
+          <React.Fragment >
+            <Typography  color="pink">Enviando seu pedido!</Typography>
+            <em >{"Ao clicar você comprará as melhores peças"}.{' '}
+            <b>{"...na melhor loja!!"} </b></em>
+          </React.Fragment>
+        }
+      >
+                <button onClick={confirmOrder}><TbCubeSend /><TbCubeSend /><TbCubeSend /></button>
+      </HtmlTooltip>
+
+
+    </div>
+
+</div>
+
+
 
 </>
 
